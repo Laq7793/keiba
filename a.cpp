ae03179f04d373c0e8f6ceafeeeeaca9cc9643b2
ibunium <90495389+ibunium@users.noreply.github.com>
2021-09-18 21:44:46 +0900

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define REP(i, n) for(int i=0;i<(n);i++)
+#define FOR(i, a, b) for(int i=(a);i<(b);i++)
+#define ALL(v) v.begin(), v.end()
+#define MOD 1000000007
+#define INF 2147483647
+
+int main(void){
+
+    return 0;
+}
