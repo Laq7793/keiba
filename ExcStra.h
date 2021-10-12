f5bfe7cfcd13fce67808370fca3b1b90e19119c6
martin-ray <54509538+martin-ray@users.noreply.github.com>
2021-10-12 17:57:38 +0900

    ExcStra2は1番人気外し

--- ExcStra.h
+++ ExcStra.h
@@ -41,10 +41,24 @@ void ExcStra1(std::vector<int>& FirstEx,
 }
 
 //一番人気を外す。特に微妙な一番人気ははずしていっていいよ。1番人気でオッズが2を下回ってるやつはゴミや
-void ExcStra2(std::vector<int>& FirstEx,
-             std::vector<int>& SecondEx,
-             std::vector<int>& ThirdEx,
-             std::vector<double>& tan)
+void ExcStra2(std::vector<int>& FirstEx,std::vector<double>& tan,int n)
 {
-    
+    double min_odds = tan[1];
+    int most_pop_baban = 1;
+
+    for(int i = 1;i<n+1;i++){
+        if(min_odds>tan[i])
+        {
+            min_odds = tan[i];
+            most_pop_baban = i;
+            std::cout << "min_odds=" << min_odds << std::endl;
+        }
+    }
+    FirstEx.push_back(most_pop_baban);
+
+}
+
+void Shinba()
+{
+
 }
